b78154d4b2f4ab03122e3ba6ff6ba26661224f0c
Ton-Thong <ton@wrmsoftware.com>
2022-03-29 22:37:43 +0700

    Hello Test

---
+++ Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,25 @@
+﻿using Kata._01_FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Tests._01_FizzBuzz
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void SayHelloTest_ShouldReturnTest()
+        {
+            // Arrange
+            var sut = new FizzBuzz();
+
+            // Act
+            var actual = sut.GetFizzBuzz();
+
+            // Assert
+            actual.Should().Be("Hello Test!");
+        }
+    }
+}
---
+++ Kata/01-FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata._01_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string GetFizzBuzz()
+        {
+            return "Hello Test!";
+        }
+    }
+}
