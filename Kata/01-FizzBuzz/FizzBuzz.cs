cdeac15efd64cad8c59293972ddbe096de170ecf
Ton-Thong <ton@wrmsoftware.com>
2022-03-29 23:28:20 +0700

    pass: add number 10 expected Buzz

--- Kata/01-FizzBuzz/FizzBuzz.cs
+++ Kata/01-FizzBuzz/FizzBuzz.cs
@@ -15,7 +15,7 @@ namespace Kata._01_FizzBuzz
                 return "Fizz";
             }
 
-            if(number == 5)
+            if(number == 5 || number == 10)
             {
                 return "Buzz";
             }
